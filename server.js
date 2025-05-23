bc723b638de87fc6045f876cc71b0df621fc11ce
rohitvhanji <56556783+rohitvhanji@users.noreply.github.com>
2025-05-23 18:42:36 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const SID = process.env.EXOTEL_SID;
+const TOKEN = process.env.EXOTEL_TOKEN;
+const FROM = process.env.FROM_NUMBER;
+const MESSAGE_URL = process.env.MESSAGE_URL; // Exotel Applet URL
+
+const numbers = [
+  "91XXXXXXXX01",
+  "91XXXXXXXX02",
+  "91XXXXXXXX03",
+  "91XXXXXXXX04",
+  "91XXXXXXXX05",
+  "91XXXXXXXX06",
+  "91XXXXXXXX07",
+  "91XXXXXXXX08",
+  "91XXXXXXXX09",
+  "91XXXXXXXX10"
+];
+
+app.post("/start-campaign", async (req, res) => {
+  try {
+    for (const to of numbers) {
+      await axios.post(
+        `https://${SID}:${TOKEN}@api.exotel.com/v1/Accounts/${SID}/Calls/connect.json`,
+        new URLSearchParams({
+          From: FROM,
+          To: to,
+          CallerId: FROM,
+          Url: MESSAGE_URL,
+          CallType: "trans"  // transactional
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        }
+      );
+    }
+    res.send("Calls initiated to all numbers.");
+  } catch (err) {
+    console.error("Error:", err.response?.data || err.message);
+    res.status(500).send("Error sending calls.");
+  }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
