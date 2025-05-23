c0657f5735270415d0f730f2fc63309e2701c4aa
rohitvhanji <56556783+rohitvhanji@users.noreply.github.com>
2025-05-23 19:41:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors"); 
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const cors = require("cors"); 
+
 
 require("dotenv").config();
 
 const app = express();
+
+
 
 app.use(cors());  
 app.use(bodyParser.json());
